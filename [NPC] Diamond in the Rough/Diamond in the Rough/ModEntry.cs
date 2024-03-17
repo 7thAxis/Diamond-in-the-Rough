22714f6c3baebe0fadd3a92e8058d9ecde20d825
7thAxis <7thAxisStudios@gmail.com>
2024-03-16 22:23:54 -0400

    JEREMY CAN WALK! AAAA!
    AAAAAAAAAAAAA!! He has a test schedule.

---
+++ [NPC] Diamond in the Rough/Diamond in the Rough/ModEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+
+namespace Diamond_in_the_Rough
+{
+    public partial class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += this.SaveLoaded; 
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.NPC), "resetForNewDay"),
+                prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.resetForNewDay_Prefix))
+            );
+        }
+        public void SaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+             Game1.getCharacterFromName("JeremyTSnail").forceOneTileWide.Set(true);
+        }
+    }
+}
---
+++ [NPC] Diamond in the Rough/Diamond in the Rough/Patches.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+using StardewValley.Buildings;
+using StardewValley.Characters;
+using StardewValley.GameData.Movies;
+using StardewValley.Locations;
+using StardewValley.Menus;
+using StardewValley.Minigames;
+using StardewValley.Monsters;
+using StardewValley.Network;
+using StardewValley.Objects;
+using StardewValley.Projectiles;
+using StardewValley.TerrainFeatures;
+using StardewValley.Tools;
+using StardewValley.Util;
+using xTile;
+using xTile.Dimensions;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+using Netcode;
+using StardewValley.Extensions;
+using HarmonyLib;
+using System.Drawing;
+using System.Security.Cryptography;
+using Color = Microsoft.Xna.Framework.Color;
+using static StardewValley.Minigames.CraneGame;
+using xTile.Layers;
+using xTile.Tiles;
+using StardewValley.ItemTypeDefinitions;
+using StardewValley.SpecialOrders;
+using System.Threading;
+
+namespace Diamond_in_the_Rough
+{
+    internal class Patches
+    {
+        private static IMonitor Monitor;
+        
+        internal static void resetForNewDay_Prefix(StardewValley.NPC __instance, int dayOfMonth)
+        {
+            try
+            {
+                if (__instance.Name == "JeremyTSnail")
+                    __instance.forceOneTileWide.Set(true);
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Failed in {nameof(resetForNewDay_Prefix)}:\n{ex}", LogLevel.Error);
+            }
+        }
+    }
+}
